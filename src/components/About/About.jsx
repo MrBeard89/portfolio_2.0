e3ff18939049693808995d8cd55ca1e4912f8d2a
Norbert Kovács <k.n.89@outlook.hu>
2023-05-14 18:24:51 +0200

    Created About.jsx + modified css in Navbar,Home.js

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/About/About.scss'
+
+export const About = () => {
+  return (
+    <div className='about-wrapper'>
+      <div className='about-container'>
+        <h1 className='introduction-title'></h1>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { About } from '../components/About/About'
 import '../styles/Home/Home.scss'
 
 import React from 'react'
 
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='home-wrapper'>
+      <About />
+    </div>
+  )
 }
