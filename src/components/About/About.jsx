855464b98983b3566e99762c0804ed2c73c1af82
Norbert Kovács <k.n.89@outlook.hu>
2023-05-17 11:38:22 +0200

    Commented About section ,Scss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+
+//Imported scss
+
 import '../../styles/About/About.scss'
+//Imported Hashlink
+
 import { HashLink } from 'react-router-hash-link'
 
 export const About = () => {
   return (
     <div className='about-wrapper' id='about'>
+      {/* Main container */}
+
       <div className='about-container'>
+        {/* Intro text container */}
+
         <div className='introduction-container'>
           <h1 className='about-title'>About me</h1>
 
@@ -16,7 +25,12 @@ export const About = () => {
         </div>
 
         <div className='about-text-container'>
+          {/* Get to know me text */}
+
           <h3 className='about-text-title'>Get to know me!</h3>
+
+          {/* About text block 1*/}
+          {/* highlighted-text - represents the bold red text  */}
 
           <p className='about-text'>
             I'm a <span className='highlighted-text'>Frontend Developer</span> building the
@@ -25,10 +39,13 @@ export const About = () => {
             <span className='highlighted-text'> Projects</span> section.
           </p>
 
+          {/* About text block 2*/}
+
           <p className='about-text'>
             I also like sharing content related to the stuff that I have learned over 1 year in
             <span className='highlighted-text'> Web Development</span> at Webler Studios so it can
             help other people of the Dev Community. Feel free to Connect or Follow me on my{' '}
+            {/* Linkedin link */}
             <a
               href={'https://www.linkedin.com/in/norbert-kov%C3%A1cs-6703ba26b/'}
               target='_blank'
@@ -38,12 +55,17 @@ export const About = () => {
             </a>{' '}
             where I post useful content related to Web Development and Programming
           </p>
+
+          {/* About text block 3*/}
+
           <p className='about-text'>
             I'm open to <span className='highlighted-text'>Job</span> opportunities where I can
             contribute, learn and grow. If you have a good opportunity that matches my skills and
             experience then don't hesitate to <span className='highlighted-text'>contact me</span>.
           </p>
         </div>
+
+        {/* Contact button */}
 
         <div className='btn-container'>
           <HashLink smooth to='#contact'>
