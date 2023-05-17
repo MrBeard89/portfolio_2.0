34f1799e21e33264b9510a0e2ca40ce29eb30472
Norbert Kovács <k.n.89@outlook.hu>
2023-05-17 11:54:33 +0200

    Commented Projects section,Scss

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,35 +6,55 @@ export const Projects = () => {
   return (
     <div className='projects-wrapper' id='projects'>
       <div className='projects-container'>
+        {/* Title */}
+
         <h1 className='projects-title'>Projects</h1>
+
+        {/* Mapping throw json file for writing less code */}
 
         {data.map((project) => {
           return (
             <div className='project' key={project.id}>
+              {/* Image container */}
+
               <div className='project-img-container'>
+                {/* Vector laptop img */}
+
                 <img
                   className='vector'
                   src={require(`../../assets/images/vectors/${project.imglinkvector}`)}
                   alt='Vector img of a laptop'
                 />
+
+                {/* Project img */}
+
                 <img
                   className='card-img'
                   src={require(`../../assets/images/card-images/${project.imglink}`)}
                   alt=''
                 />
               </div>
+
+              {/* Main wrapper for information about each project */}
+
               <div className='project-info-wrapper'>
+                {/* Info container - Title, Text ,Note */}
+
                 <div className='info-container'>
                   <h3 className='info-title'>{project.title}</h3>
                   <p className='info-text'>{project.description}</p>
                   <p className='info-note'>{project.note}</p>
                 </div>
 
+                {/* Button container for tech used for projects */}
+
                 <div className='btn-container'>
                   {project.tech.map((tech, index) => {
                     return <button key={index}>{tech}</button>
                   })}
                 </div>
+
+                {/* Links container - Code and Live buttons */}
 
                 <div className='links-container'>
                   <Link className='project-link' to={project.projectlink} target='_blank'>
